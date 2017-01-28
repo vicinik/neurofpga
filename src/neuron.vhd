f1fc724b5868b2d8769ae2da0be1b756a50847dc
nhami <nhami@DESKTOP-74TSDA5>
2017-01-28 12:12:50 +0100

    Added neuron and Readme

---
+++ src/neuron.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164;
+use ieee.math_real;
+use work.Global.all;
+
+entity Neuron is
+
+end entity;
+
+architecture Bhv of Neuron is
+	
+begin
+	
+end architecture;
