8a73a30955e51a7b85facb824ff5935f06350d60
nhami <nhami@DESKTOP-74TSDA5>
2017-01-28 12:09:40 +0100

    First file structure

---
+++ src/layer.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164;
+use ieee.math_real;
+use work.Global.all;
+
+entity Layer is
+
+end entity;
+
+architecture Bhv of Layer is
+	
+begin
+	
+end architecture;
---
+++ src/net.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164;
+use ieee.math_real;
+use work.Global.all;
+
+entity Net is
+
+end entity;
+
+architecture Bhv of Net is
+	
+begin
+	
+end architecture;
